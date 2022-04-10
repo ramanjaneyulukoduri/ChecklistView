3c72bfef313a3467fc752fef2b5e291e5472cfa9
ramanjaneyulukoduri <ramanjaneyulu.koduri@griffithuni.edu.au>
2022-04-10 15:00:33 +0530

    Initial Commit for Project setup

---
+++ Checklist/Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Ajay Girolkar on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
